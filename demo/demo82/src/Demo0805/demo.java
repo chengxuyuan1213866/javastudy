d6c1ed6af2af0224552c727cb90c4a53e926835c
zhangxingcheng <1919404416@qq.com>
2023-08-08 12:01:29 +0800

    冒泡排序

--- demo/demo82/src/Demo0805/demo.java
+++
@@ -1,4 +0,0 @@
-package src.Demo0805;
-
-public class demo {
-}
--- demo/demo82/src/Demo0807/Demo7.java
+++ demo/demo82/src/Demo0807/Demo7.java
@@ -2,7 +2,7 @@ package src.Demo0807;
 
 public class Demo7 {
     public static void main(String[] args) {
-        int[] arr= {5,6,4,7};
+        int[] arr= {5,6,4,7,5};
         int num = 0;
         for (int i = 0;i<arr.length;i++)
         {
