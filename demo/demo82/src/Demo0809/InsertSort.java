9fb95eb84e8131d4038b69ae09248b036b2446eb
zhangxingcheng <1919404416@qq.com>
2023-08-09 14:12:38 +0800

    复习插入排序

---
+++ demo/demo82/src/Demo0809/InsertSort.java
@@ -0,0 +1,18 @@
+package src.Demo0809;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //
+        // 插入排序
+        for (int i = 1; i < arr.length; i++) {
+            int num = arr[i];
+            int index = i - 1;
+            while (index >= 0 && arr[index] < num) {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = num;
+        }
+    }
+}
