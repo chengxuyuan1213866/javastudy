8d516f31b81d4636a81597c329e5351364f204a8
zhangxingcheng <1919404416@qq.com>
2023-08-09 20:50:03 +0800

    杨辉三角

---
+++ demo/demo82/src/Demo0809/Task/Demo3.java
@@ -0,0 +1,38 @@
+package src.Demo0809.Task;
+
+public class Demo3 {
+    public static void main(String[] args)
+    {
+        int n = 5;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || j == n-1)
+                {
+                    arr[i][j]=1;
+                }
+                else {
+                    if (i>=1)
+                    {
+                        arr[i][j] = arr[i-1][j]+arr[i-1][j+1];
+                    }
+                }
+
+
+
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j =arr[0].length-1; j >=0 ; j--) {
+                if (arr[i][j]==0)
+                {
+                    break;
+                }
+                System.out.print(arr[i][j]);
+                //System.out.printf("%d%d\t",i,j);
+            }
+            System.out.println();
+        }
+    }
+
+}
