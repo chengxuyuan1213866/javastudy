1980014b3bde62296fb88ec9a431ff2860717657
zhangxingcheng <1919404416@qq.com>
2023-08-11 20:56:40 +0800

    将数组左移

---
+++ demo/demo82/src/Demo0811/Task/Demo7.java
@@ -0,0 +1,23 @@
+package src.Demo0811.Task;
+
+import java.util.Arrays;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr1 = new int[arr.length];
+        int k = 3;
+        arr1 = LeftShift(arr,k);
+        System.out.println(Arrays.toString(arr1));
+
+
+
+    }
+    public static int[] LeftShift(int[] arr,int k)
+    {
+        int[] arr1 = new int[arr.length];
+        System.arraycopy(arr,k,arr1,0,arr.length-k);
+        System.arraycopy(arr,0,arr1,arr.length-k,k);
+        return arr1;
+    }
+}
