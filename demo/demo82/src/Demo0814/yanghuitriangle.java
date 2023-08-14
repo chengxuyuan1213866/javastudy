ca6137c506a06f274ac7c9fedd712152c3a143b1
zhangxingcheng <1919404416@qq.com>
2023-08-14 13:58:22 +0800

    杨辉三角

---
+++ demo/demo82/src/Demo0814/yanghuitriangle.java
@@ -0,0 +1,33 @@
+package src.Demo0814;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Administrator
+ */
+public class yanghuitriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[][] arr = triangle(num);
+        for (int[] ints : arr) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+    public static int[][] triangle(int row){
+        int[][] triangle = new int[row][];
+
+        for (int i = 0; i < triangle.length; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1; // 第一个
+            triangle[i][i] = 1; // 最后一个
+            for (int j = 1; j < i ; j++) {
+                triangle[i][j] = triangle[i - 1][j] + triangle[i -1][j - 1];
+            }
+        }
+
+        return triangle;
+    }
+}
