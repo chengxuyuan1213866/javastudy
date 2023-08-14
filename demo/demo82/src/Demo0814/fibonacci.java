638cd2de6d114f268b44a4f2b6b694782a48417f
zhangxingcheng <1919404416@qq.com>
2023-08-14 15:21:31 +0800

    递归实现斐波那契数列

---
+++ demo/demo82/src/Demo0814/fibonacci.java
@@ -0,0 +1,19 @@
+package src.Demo0814;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int a = fun(6);
+        System.out.println(a);
+    }
+    public static int fun(int n) {
+        if (n == 2)
+        {
+            return 1;
+        }
+        else if (n == 1){
+            return 1;
+        }
+
+        return fun(n-1)+fun(n-2);
+    }
+}
--- demo/demo82/src/Demo0814/yhtriangle.java
+++ demo/demo82/src/Demo0814/yhtriangle.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 /**
  * @author Administrator
  */
-public class yanghuitriangle {
+public class yhtriangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
