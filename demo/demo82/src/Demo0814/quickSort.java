af793da361cbb5ba43d0883da504424b133a2349
zhangxingcheng <1919404416@qq.com>
2023-08-14 19:34:48 +0800

    快速排序，方法实现

--- demo/demo82/src/Demo0814/quickSort.java
+++ demo/demo82/src/Demo0814/quickSort.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
  */
 public class quickSort {
     public static void main(String[] args) {
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        for (int i = 0; i < arr.length/2; i++) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+/*        for (int i = 0; i < arr.length/2; i++) {
             int key = i;
             int left = 0;
             int right = arr.length-1;
@@ -31,6 +31,35 @@ public class quickSort {
             arr[key] = tem;
 
         }
+        System.out.println(Arrays.toString(arr))*/;
+        sort(arr,0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
+    public static void sort(int[] arr,int start,int end){
+        if (start >= end){
+            return;
+        }
+        int left = start;
+        int right = end;
+        int key = arr[left];
+        while (left<right)
+        {
+            while (left<right && arr[right]<key){
+                right--;
+            }
+            while (left<right && arr[left]>key){
+                left++;
+            }
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+        key = arr[left];
+        arr[left] = key;
+
+        sort(arr, start,left-1);
+        sort(arr, left+1, end);
+
+    }
+
 }
