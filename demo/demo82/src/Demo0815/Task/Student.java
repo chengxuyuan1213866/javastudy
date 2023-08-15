ed3411a5d70f513eedf3fb01881ff3f319764fa6
zhangxingcheng <1919404416@qq.com>
2023-08-16 00:34:00 +0800

    作业

---
+++ demo/demo82/src/Demo0815/Task/Student.java
@@ -0,0 +1,13 @@
+package src.Demo0815.Task;
+
+public class Student {
+    String name = "蔡徐坤";
+    char gender = '鸡';
+    int age = 25;
+    int id = 25;
+    double score = 2.5;
+    public String say()
+    {
+        return name + gender + age + id + score;
+    }
+}
