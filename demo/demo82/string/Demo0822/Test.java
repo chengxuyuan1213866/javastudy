b7e85a1edcf5bd88adf598b3b2a62ef601397386
zhangxingcheng <1919404416@qq.com>
2023-08-22 21:55:53 +0800

    笔记

--- demo/demo82/string/Demo0822/Test.java
+++ demo/demo82/string/Demo0822/Test.java
@@ -1,12 +1,11 @@
-package opp.Demo0822;
+package string.Demo0822;
 
 public class Test {
     public static void main(String[] args) {
-        char[] chs={'你','\0','好'};
-        String a = "你把啥似的\0手打进度款";
-        String str = new String(chs);
-        String str1 = new String(a);
-        System.out.println(str1);
+        // 文本块声明
+        String str = """
+                高尔基说:"书籍是人类进步的阶梯"。
+                """;
         System.out.println(str);
     }
 }
