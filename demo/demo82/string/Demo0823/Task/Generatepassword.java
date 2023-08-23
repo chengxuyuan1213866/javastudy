bea9e246dd58d42ec91cd2dc7bae460b267fb9c8
zhangxingcheng <1919404416@qq.com>
2023-08-23 21:05:33 +0800

    作业：生成6位数随机密码

---
+++ demo/demo82/string/Demo0823/Task/Generatepassword.java
@@ -0,0 +1,37 @@
+package string.Demo0823.Task;
+
+import java.util.Random;
+
+public class Generatepassword {
+    public static void main(String[] args) {
+        System.out.println(get());
+
+    }
+    public static String get()
+    {
+        Random random = new Random();
+        String stu = new String();
+        for (int i = 0; i < 6; i++) {
+            Integer a = random.nextInt(10);
+            String c = a.toString();
+            if (!stu.contains(c))
+            {
+                if (i == 0)
+                {
+                    int b = random.nextInt(65,91);
+                    stu += (char)b;
+                    continue;
+                }
+                stu += a;
+            }
+            else {
+                i--;
+            }
+
+        }
+
+        return stu;
+
+
+    }
+}
