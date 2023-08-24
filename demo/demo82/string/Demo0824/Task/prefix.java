41aaa5f296f86d902be3a9d5b65d102c60b24190
zhangxingcheng <1919404416@qq.com>
2023-08-24 23:26:17 +0800

    最长公共前缀

---
+++ demo/demo82/string/Demo0824/Task/prefix.java
@@ -0,0 +1,35 @@
+package string.Demo0824.Task;
+
+public class prefix {
+    public static void main(String[] args) {
+        String[] s = {"flower","flow","flight"};
+        System.out.println(fun(s));
+    }
+    public static int fun(String[] stu)
+    {
+        int count = 0;
+        int count1 = 0;
+        int min = 0;
+        for (int i = 0; i < stu.length; i++) {
+            if (stu[i].length()<stu[min].length()){
+                min = i;
+            }
+        }
+        int n = stu[min].length();
+        for (int i = 0; i < n; i++) {  // 便利数组内最短元素的遍历
+            for (int j = 0; j < stu.length; j++) {
+                if (stu[min].charAt(i) == stu[j].charAt(i))
+                {
+                    count++;
+
+                }
+            }
+            if (count == stu.length)
+            {
+                count1++;
+            }
+            count =0;
+        }
+        return count1;
+    }
+}
