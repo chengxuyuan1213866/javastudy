638342bd0304423ad2099edbefccd125a9a2df61
zhangxingcheng <1919404416@qq.com>
2023-08-25 21:11:44 +0800

    正则

---
+++ demo/demo82/string/Demo0825/Regular.java
@@ -0,0 +1,12 @@
+package string.Demo0825;
+
+public class Regular {
+    public static void main(String[] args) {
+        // 正则表达式
+        String s = "8";
+        String s1 = "[0-5[7-9]]";
+        boolean b = s.matches(s1);
+        System.out.println(b);
+
+    }
+}
