62684b3ad582327aa1e21d2e42fc49ca0729047b
zhangxingcheng <1919404416@qq.com>
2023-08-25 17:16:46 +0800

    输出字母行号

--- demo/demo82/string/Demo0824/Task/TabularName.java
+++ demo/demo82/string/Demo0824/Task/TabularName.java
@@ -2,15 +2,16 @@ package string.Demo0824.Task;
 
 public class TabularName {
     public static void main(String[] args) {
-        System.out.println(name(100));
+        System.out.println(name(26));
     }
     public static String name(int c)
     {
 
         StringBuilder stu = new StringBuilder();
+        c-=1;
         while (c != 0)
         {
-            stu.append((char) (64+ c%26));
+            stu.append((char) (65+ c%26));
             c /= 26;
         }
         return stu.reverse().toString();
