d049f21cb59d4b235fc74dc8396bf6b6d766bab1
chengxuyuan1213866 <1919404416@qq.com>
2023-08-28 17:31:17 +0800

    调用不到1111 play方法

---
+++ demo/demo82/com/kfm/code/test/Demo0828/Test.java
@@ -0,0 +1,29 @@
+package com.kfm.code.test.Demo0828;
+
+public class Test {
+    static {
+        int a = 1;
+    }
+    static class A
+    {
+        static int a = 2;
+        public static void play()
+        {
+            System.out.println("A类的 play方法 ");
+        }
+    }
+    static class Test1
+    {
+        A aaa = new A(){
+            public static void play()
+            {
+                System.out.println("1111 play方法 ");
+            }
+        };
+    }
+    public static void main(String[] args) {
+        Test t = new Test();
+        A t2 = new A();
+        Test1 t1 = new Test1();
+    }
+}
