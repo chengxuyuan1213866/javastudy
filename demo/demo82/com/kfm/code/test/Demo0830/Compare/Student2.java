1213571ef31feb38fbecee2751deb42347a2c3eb
chengxuyuan1213866 <1919404416@qq.com>
2023-08-31 16:15:50 +0800

    comparator接口实现比较器

---
+++ demo/demo82/com/kfm/code/test/Demo0830/Compare/Student2.java
@@ -0,0 +1,56 @@
+package code.test.Demo0830.Compare;
+
+
+import java.util.Comparator;
+
+/**
+ * comparator接口实现比较器
+ * 比较器
+ */
+public class Student2 extends Student implements Comparator {
+
+    Student2(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+       return 0;
+}
+
+
+}
+
+class Test2222 {
+    public static void main(String[] args) {
+
+         Student2 stu[] = {
+                new Student2("蔡徐坤", 25),
+                new Student2("蔡虚坤", 250),
+                new Student2("蔡虚鲲", 2500),
+        };
+        Comparator c = (o1, o2) ->
+        {
+            if (o1 instanceof Student2 stu1 && o2 instanceof Student2 stu2) {
+                if (stu1.getName().equals(stu2.getName()) && stu1.getAge() == stu2.getAge()) {
+                    return 0;
+                } else if (stu1.getAge() > stu2.getAge()) {
+                    return 1;
+                } else if (stu1.getName().compareTo(stu2.getName()) > 0) {
+                    return 1;
+                } else
+                    return -1;
+            } else throw new RuntimeException("输入对象有误");
+
+        };
+        System.out.println(c.compare(stu[0], stu[1]));
+
+
+
+    }
+
+}
+
+
+
+
