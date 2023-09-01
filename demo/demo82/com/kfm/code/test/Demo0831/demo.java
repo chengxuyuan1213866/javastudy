32b19b323c88757af241c372902639633a076cb0
chengxuyuan1213866 <1919404416@qq.com>
2023-09-01 15:44:59 +0800

    斐波那契

--- demo/demo82/com/kfm/code/test/Demo0831/demo.java
+++ demo/demo82/com/kfm/code/test/Demo0831/demo.java
@@ -1,4 +1,26 @@
 package code.test.Demo0831;
 
 public class demo {
+    public static void main(String[] args) {
+        System.out.println(fun(20));
+    }
+    public static int fun(int n)
+    {
+        if (n <=2) {
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return fun(n-1);
+            }
+        }
+        else {
+            return fun(n-1)+fun(n-2);
+        }
+        return 1;
+    }
+}
+interface AA
+{
+
 }
