93e21fff79bdd0e278d5e1becea5f6365cd30494
chengxuyuan1213866 <1919404416@qq.com>
2023-09-05 21:30:07 +0800

    差集运算

---
+++ demo/demo82/com/kfm/code/CodeTest/Demo0905/Task/Demo3.java
@@ -0,0 +1,30 @@
+package code.CodeTest.Demo0905.Task;
+
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * 差集计算
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        List<String> list1 = List.of("a", "b", "c");
+        List<String> list2 = List.of("b", "c", "d");
+        Vector a = DifferenceSet(list1,list2);
+        System.out.println(a);
+    }
+    public static Vector DifferenceSet(List l1,List l2)
+    {
+        Vector vector = new Vector();
+        for (int i = 0; i < l1.size(); i++) {
+            if (l2.contains(l1.get(i)))
+            {
+                continue;
+            }
+            vector.add(l1.get(i));
+        }
+        return vector;
+    }
+
+}
