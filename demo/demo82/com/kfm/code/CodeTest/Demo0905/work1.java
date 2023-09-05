c9d73a8efba07c7a2d8768989fb026c66e97eb94
chengxuyuan1213866 <1919404416@qq.com>
2023-09-05 21:17:24 +0800

    去重统计

---
+++ demo/demo82/com/kfm/code/CodeTest/Demo0905/work1.java
@@ -0,0 +1,27 @@
+package code.CodeTest.Demo0905;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class work1 {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList();
+        al.add("aaa");
+        al.add("11");
+        al.add("22");
+        al.add("33");
+        al.add("44");
+        al.add("55");
+        al.add("66");
+        LinkedList<String> ll = new LinkedList();
+        ll.add("aaa");
+        ll.add("11");
+        ll.add("22");
+        ll.add("33");
+        ll.add("44");
+        ll.add("55");
+        ll.add("66");
+        System.out.println(al);
+        System.out.println(ll);
+    }
+}
