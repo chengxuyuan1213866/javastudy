f4cae8651e417e5de89ee32ca9bbecb892b2501d
chengxuyuan1213866 <1919404416@qq.com>
2023-09-07 00:46:10 +0800

    集合梳理

--- demo/demo82/com/kfm/code/CodeTest/Demo0906/Task/HashTableTest.java
+++ demo/demo82/com/kfm/code/CodeTest/Demo0906/Task/HashTableTest.java
@@ -11,7 +11,7 @@ public class HashTableTest {
         table.put("w","蔡虚鲲");
         table.put("t","蔡坤坤");
         table.put("y","蔡坤坤");
-
+        table.put("1","dsad");
         // 具有和迭代器相同的作用，但是不具备迭代器的删除功能
         Enumeration enumeration = table.keys();
         System.out.println(table);
